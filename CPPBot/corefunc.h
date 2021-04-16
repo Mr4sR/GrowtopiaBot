81a19c60adde85edaf6dcdeb8b8f2a20d0971e39
Mr4sR <71956839+Mr4sR@users.noreply.github.com>
2021-04-16 13:56:46 +0700

    Update corefunc.h

--- CPPBot/corefunc.h
+++ CPPBot/corefunc.h
@@ -16,7 +16,7 @@
 //    You should have received a copy of the GNU Affero General Public License
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#include "enet/enet.h"
+#include "enet/include/enet.h"
 #include <string>
 #include <conio.h>
 #include <iostream>
